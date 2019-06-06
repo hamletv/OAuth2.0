36c472032cc8cc797696bd6a2d95485e6698e980
Hamlet <hamlet.villa@gmail.com>
2019-06-06 19:21:37 +0000

    database with new user class

--- database_setup.py
+++ database_setup.py
@@ -2,34 +2,41 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    email = Column(String(80), nullable = False)
+    picture = Column(String(250))
+
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-   
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    # foregin key relationship between user class and restaurant class
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-    @property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-       }
- 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
-
-
-    name =Column(String(80), nullable = False)
+    # create database with sqlalchemy: map python objects to columns in our db
+    # sytax: columnName = Column(attributes)
+    name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
+    course = Column(String(250))
     description = Column(String(250))
     price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+    # foregin key relationship between restaurant class and menuitems class
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    # foregin key relationship between user class and menuitems class
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
     @property
@@ -45,7 +52,7 @@ class MenuItem(Base):
 
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
- 
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+
 
 Base.metadata.create_all(engine)
